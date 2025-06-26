3169810f66998f5bf0d30c9afa4ad47fbe00a356
Suvarna2025 <suvarnaravi20@gmail.com>
2025-06-26 14:37:01 +0530

    completed

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p className='text-center py-2'>Developed By Suvarna </p>
+    </footer>
+  )
+}
+
+export default Footer
